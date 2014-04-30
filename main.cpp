0487b4cf3381c075f980ea0a9f6aefba6a03ec2c
Xuanyi Li <xli19@nd.edu>
2014-04-30 15:58:19 -0400

    main.cpp clean up

--- main.cpp
+++ main.cpp
@@ -21,39 +21,14 @@
 using namespace std;
 
 int main() {
-    // test loading file with multiple sequences
-    //ExtractSequence testSequences("ultimateORFS18608.fasta");
-    //Alignment testPair(testSequences);
-    //testPair.computeTables();
-    //testPair.updateAlign();
-    //testPair.printAlignment();
-    //testSequences.printSequences(0);
-    //cout << "++++++++++++++++++++++++++++++++++++" << endl;
-    //testSequences.printSequences(1);
-    //cout << "++++++++++++++++++++++++++++++++++++" << endl;
+    // Alignment
     Alignment testAlign("ultimateORFS3710.fasta");
     testAlign.alignAll();
     testAlign.printAlignment();
-    AlignmentCIGAR testCigar("ultimateORFS3710_aligned.fasta"); 
+    // CIGAR string
+    AlignmentCIGAR testCigar("ultimateORFS3710_aligned.fasta");
     testCigar.setCigar();
     testCigar.printCigar();
-    //testAlign.printTables(); // TEST
-//    testCigar.setCigar();
-//    testCigar.printCigar();
-    //NeighborJoining testNJ("ultimateORFS18608.fasta");
-    //testNJ.write_distance();
-    //testNJ.print_distance();
-
-
-//    ExtractSequence testSequences("nucSampleAlignment.fa");
-//    testSequences.printSequences();
-  
-  // testing to verify the frequency calculations
-//  ExtractSequence codFreqTest("Ecol_test.fasta.short");
-//  ExtractSequence codFreqTest("Ecol_test.fasta");
-  //ExtractSequence codFreqTest("Ypes.fasta");
-//  ExtractSequence codFreqTest("Ecol_test.fasta.short.really");
-//  codFreqTest.printSequences();
   ExtractSequence codFreq("Ypes.fasta");
   MinMax calcMinMax("Ypes.fasta", codFreq.getVectorOfSequences());
 	return 0;
